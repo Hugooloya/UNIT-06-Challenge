71a4e976af10d10bb8f0f19cdb2f6c31474e0818
Hugo <hugooloya@gmail.com>
2022-11-08 14:10:11 -0700

    HTML and CSS updated
    Crucial updates for HTML and CSS pages

--- script.js
+++ script.js
@@ -10,48 +10,15 @@ let weather = {
       const { icon, description } = data.list[0].weather[0];    
       const { temp, humidity } = data.list[0].main;    
       const { speed } = data.list[0].wind;  
-      console.log(data);  
-  },
+      console.log(data);
+      document.querySelector('.city').innerText = name;
+      document.querySelector('.icon').src = "https://openweathermap.org/img/wn/" + icon +"@2x.png";
+      document.querySelector('.description').innerText = description;
+      document.querySelector('.temp').innerText = temp + "°F";
+      document.querySelector('.humidity').innerText = humidity;
+      document.querySelector('.speed').innerText = speed;
+
+
+    },
   };
   
-
-
-
-//   var cityForm = document.querySelector('#search-form');
-
-//   function searchCityForm(event) {
-//       event.preventDefault();
-      
-//       var citySearchVal = document.querySelector('#cityName').value;
-      
-      
-//       if (!citySearchVal) {
-//           console.error('Please enter a city');
-//           return;
-//       }
-  
-//       var cityCoords = "http://api.openweathermap.org/geo/1.0/direct?q="
-//       + citySearchVal
-//       + '&limit=1&appid=6e488f3637f1e65cabedb26e8fc39da1'
-  
-//       fetch(cityCoords)
-//       .then(function (response) {
-//           return response.json();
-//       })
-//       .then(function (data) {
-//           latitude = data[0].lat
-//           longitude = data[0].lon
-//                     console.log(latitude);
-  
-//     //       var fullApi = "api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=6e488f3637f1e65cabedb26e8fc39da1"
-  
-//     //       fetch(fullApi)
-//     //       .then(function (response) {
-//     //           return response.json();
-//     //       })
-//     //       .then(function (data) {
-//     //       })
-//       })      
-//   } 
-  
-//   cityForm.addEventListener('submit', searchCityForm);
